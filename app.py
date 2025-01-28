5d4f2cf22e4be6583b3dfc9ef37cdc3ef73dca6a
wokeii <137532823+stebi123@users.noreply.github.com>
2025-01-28 21:24:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -138,4 +138,6 @@ def create_tables():
         db.session.commit()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Get the PORT from the environment variable or default to 5000
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
